8dede4a93800f8cd55abd0322b78e264827d7de1
versroot <131954932+versroot@users.noreply.github.com>
2024-07-16 14:23:10 +0200

    all 3 tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,15 @@
-function getElementWidth(content, padding, border) {
-  return (
-    parseFloat(content, 10) +
-    2 * (parseFloat(padding, 10) + parseFloat(border, 10))
-  );
+function filterArray(numbers, value) {
+  let arrayGreaterThanValue = [];
+  for (let number of numbers) {
+    if (number > value) {
+      arrayGreaterThanValue.push(number);
+    }
+  }
+  return arrayGreaterThanValue;
 }
 
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
